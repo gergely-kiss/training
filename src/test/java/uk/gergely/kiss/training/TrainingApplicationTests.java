33b920a7b85d6293df167f02df4a33ce72943c90
gergely kiss <kiss-gergely@outlook.com>
2020-04-07 11:05:31 +0100

    init commit

---
+++ src/test/java/uk/gergely/kiss/training/TrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.gergely.kiss.training;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
