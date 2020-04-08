cd3366214d9e7faf16bfb885ce3f63b6b488bdee
gergely kiss <kiss-gergely@outlook.com>
2020-04-08 17:22:20 +0100

    Saving progress

--- src/main/java/uk/gergely/kiss/training/tutorials/springboot/batch/GreetingBatchComponent.java
+++ src/main/java/uk/gergely/kiss/training/tutorials/springboot/batch/GreetingBatchComponent.java
@@ -3,10 +3,12 @@ package uk.gergely.kiss.training.tutorials.springboot.batch;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import uk.gergely.kiss.training.tutorials.springboot.service.GreetingService;
 
+@Profile("batch")
 @Component
 public class GreetingBatchComponent {
     private static final Logger LOGGER = LoggerFactory.getLogger(GreetingBatchComponent.class);
@@ -19,14 +21,14 @@ public class GreetingBatchComponent {
         this.greetingService = greetingService;
     }
 
-    @Scheduled(cron = "0,30 * * * * *") // run the job at 0 and 30 second mark of every minute of every hour of every day
+    @Scheduled(cron = "${batch.greeting.cron}") // run the job at 0 and 30 second mark of every minute of every hour of every day
     public void cronJob() {
         LOGGER.info("cronJob started");
         LOGGER.info("cronJob: there are {} greetings in the data store.", greetingService.findAll().size());
         LOGGER.info("cronJob finished");
     }
 
-    @Scheduled(initialDelay = 5000, fixedRate = 15000) // start the job every fixedRate defined time
+    @Scheduled(initialDelayString = "${batch.greeting.initialdelay}", fixedRateString = "${batch.greeting.fixedrate}") // start the job every fixedRate defined time
     public void fixedRateJobWithInitialDelay() {
         LOGGER.info("fixedRateJobWithInitialDelay started");
         long pause = 5000;
@@ -40,7 +42,7 @@ public class GreetingBatchComponent {
         LOGGER.info("fixedRateJobWithInitialDelay finished");
     }
 
-    @Scheduled(initialDelay = 5000, fixedDelay = 15000) // starts the job every time after the previous job finished delayed with the fixed Delay time ergo at most one instance of the job is running in any given time
+    @Scheduled(initialDelayString = "${batch.greeting.initialdelay}", fixedDelayString = "${batch.greeting.fixeddelay}") // starts the job every time after the previous job finished delayed with the fixed Delay time ergo at most one instance of the job is running in any given time
     public void fixedDelayJobWithInitialDelay() {
         LOGGER.info("fixedDelayJobWithInitialDelay started");
         long pause = 5000;
